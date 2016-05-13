f638a8e0857df0b4d0816baa8551c506454c1302
bengeos <george.beng@gmail.com>
2016-05-12 23:56:00 -0700

    update the sdc_mode module with the new ip_camera module

--- Tests/BEN/Final_GUI/SDC_Mode.py
+++ Tests/BEN/Final_GUI/SDC_Mode.py
@@ -1,13 +1,15 @@
 import MLP as net
+import IP_Camera as cam
 import cv2
-import urllib2
 import numpy as np
 import serial as sp
+import threading
 import time
 
 class SDC_Mode(object):
     def __init__(self,NetLayers,ip_camera_host,capture_image_size,serialport):
         self.MyNet = net.MLP(NetLayers)
+        self.Camera = cam.Camera(ip_camera_host)
         self.Host = ip_camera_host
         self.Frame = None
         self.Image = None
@@ -15,16 +17,38 @@ class SDC_Mode(object):
         self.Port = serialport
         self.Speed = 1
         self.SerialPort = sp.Serial(self.Port)
-    def Connect_Serial(self):
-        print 'Connecting to Serial Port: '+self.Port
+        self.SerialThread = threading.Thread(target=self.ReadWriteSerial,args=())
+        self.SerialWrite = None
+        self.SerialRead = None
+        self.WheelState = "Stopped"
+        self.SerialThread.start()
+    def Update_WheelState(self,val):
+        if(val == 0):
+            self.WheelState = "Stopped"
+        if(val == 1):
+            self.WheelState = "Moving Forward"
+        if(val == 2):
+            self.WheelState = "Moving Backward"
+        if(val == 3):
+            self.WheelState = "Moving Right"
+        if(val == 4):
+            self.WheelState = "Moving Left"
     def Disconnect_Serial(self):
         self.SerialPort.close()
     def ChangeSpeed(self,new_speed):
         self.Speed = new_speed
         self.SerialPort.write(new_speed)
+    def ReadWriteSerial(self):
+        prev = ''
+        while(self.SerialPort.isOpen()):
+            self.SerialRead = self.SerialPort.readline()
+            print self.SerialRead
+            time.sleep(0.01)
+
     def Send_Serial(self,data):
         print 'Sending Serial Data: '+str(data)
         self.SerialPort.write(str(data))
+        #self.SerialWrite = str(data)
     def Load_Trained_MLP(self,mlp_xml_file):
         print 'Loading trained data'
         self.MyNet.Load_From(mlp_xml_file)
@@ -40,40 +64,37 @@ class SDC_Mode(object):
             self.Send_Serial('A')
         if(data == 5):
             self.Send_Serial('S')
+        else:
+            self.Send_Serial('S')
     def Start_Driving(self):
-        print 'Start self driving mode'
-        self.Stream = urllib2.urlopen(self.Host)
-        bytes=''
+        self.Camera.start()
         self.isRunning = True
         while(self.isRunning):
-            bytes+=self.Stream.read(512)
-            a = bytes.find('\xff\xd8')
-            b = bytes.find('\xff\xd9')
-            if a!=-1 and b!=-1:
-                jpg = bytes[a:b+2]
-                bytes= bytes[b+2:]
-                self.Frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.CV_LOAD_IMAGE_GRAYSCALE)
-                x = np.size(self.Frame)
-                if(x >= self.Size[0]*self.Size[1]):
-                    try:
-                        self.Image = cv2.resize(self.Frame,self.Size)
-                        ImageArray = self.Image.reshape(-1,self.MyNet.Network_Shape[0]).astype(np.float32)
-                        _TrainingData = []
-                        _TestingData = []
-                        _TestingData.append(1)
-                        _TrainingData.append(ImageArray)
-                        TestingData = [np.reshape(x, (self.MyNet.Network_Shape[0], 1)) for x in _TrainingData]
-                        TestingResult = [np.reshape(x, (1, 1)) for x in _TestingData]
-                        TestData = zip(TestingData,TestingResult)
-                        res = self.MyNet.Evaluate_Data(TestData)
-                        print 'Capture Result: '
-                        print res
-                        self.Drive_Serial(res[0])
-                        cv2.imshow('IP Camera '+self.Host,self.Frame)
-                    finally:
-                        x = 0
-                k = cv2.waitKey(1)
-                if(k == 27):
-                    break
+            self.Frame = self.Camera.get_image()
+            x = np.size(self.Frame)
+            if(x >= self.Size[0]*self.Size[1]):
+                try:
+                    self.Image = cv2.resize(self.Frame,self.Size)
+                    ImageArray = self.Image.reshape(-1,self.MyNet.Network_Shape[0]).astype(np.float32)
+                    _TrainingData = []
+                    _TestingData = []
+                    _TestingData.append(1)
+                    _TrainingData.append(ImageArray)
+                    TestingData = [np.reshape(x, (self.MyNet.Network_Shape[0], 1)) for x in _TrainingData]
+                    TestingResult = [np.reshape(x, (1, 1)) for x in _TestingData]
+                    TestData = zip(TestingData,TestingResult)
+                    res = self.MyNet.Evaluate_Data(TestData)
+                    print 'Process Result: '+str(res[0])
+                    self.Update_WheelState(res[0])
+                    self.Drive_Serial(res[0])
+                    cv2.imshow('IP Camera '+self.Host,self.Frame)
+                    self.Drive_Serial(0)
+                finally:
+                    pass
+            k = cv2.waitKey(1000)
+            if(k == 27):
+                self.Update_WheelState(0)
+                self.Camera.Stop()
+                break
         cv2.destroyAllWindows()
 
